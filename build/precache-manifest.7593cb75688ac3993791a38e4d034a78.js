5bd35976b2ce00c03875ecdbf18b112b0d847664
Alex <you@example.com>
2021-09-16 23:55:11 +0300

    после деплоя

---
+++ build/precache-manifest.7593cb75688ac3993791a38e4d034a78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f511d77524edd47cf8aee977fe9817",
+    "url": "/restservice/index.html"
+  },
+  {
+    "revision": "ce3973e851ad86fc65c6",
+    "url": "/restservice/static/css/main.4edb5e80.chunk.css"
+  },
+  {
+    "revision": "6e6bde9492d076aa75ba",
+    "url": "/restservice/static/js/2.49dd3654.chunk.js"
+  },
+  {
+    "revision": "ce3973e851ad86fc65c6",
+    "url": "/restservice/static/js/main.1d19fde5.chunk.js"
+  },
+  {
+    "revision": "2d82ad1300b23c8544aa",
+    "url": "/restservice/static/js/runtime~main.a8494893.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/restservice/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/restservice/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
